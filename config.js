12876ec5ef4011dcb473c4c247dff5242631eabb
Camilovv25 <115130294+Camilovv25@users.noreply.github.com>
2023-03-03 22:02:20 -0500

    features: change port

--- config.js
+++ config.js
@@ -31,7 +31,7 @@ const configs = {
             //? Aqui deberan estar las configuraciones para la conexion con sequelize
             dialect: 'postgres',
             host: process.env.DB_HOST,
-            port: process.env.DB_HOST,
+            port: process.env.DB_PORT,
             username: process.env.DB_USER,
             password: process.env.DB_PASSWORD,
             database: process.env.DB_NAME,
